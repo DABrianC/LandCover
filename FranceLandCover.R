be329a23226b9f4d01c4bb74e8784c634b48c705
Brian Calhoon <bcalhoon@msi-inc.com>
2021-11-15 15:59:31 -0500

    script

--- FranceLandCover.R
+++ FranceLandCover.R
@@ -17,25 +17,24 @@ MODIStsp_get_prodlayers("MCD12Q1")
 
 map_boundary <- geoboundaries("France")
 
-plot(map_boundary)
-
 # Defining filepath to save downloaded spatial file
 spatial_filepath <- "LandCoverData/france.shp"
-# Saving downloaded spatial file on to our computer
+# Saving downloaded spatial file on  our computer
 st_write(map_boundary, paste0(spatial_filepath))
 
-MODIStsp(gui             = FALSE,
-         out_folder      = "LandCoverData",
-         out_folder_mod  = "LandCoverData",
-         selprod         = "LandCover_Type_Yearly_500m (MCD12Q1)",
-         bandsel         = "LC1", 
-         user            = "briancalhoon" ,
-         password        = "00h0OqKWDw$67R",
-         start_date      = "2020.01.01", 
-         end_date        = "2020.12.31", 
-         verbose         = FALSE,
-         spatmeth        = "file",
-         spafile         = spatial_filepath,
-         out_format      = "GTiff")
+MODIStsp(gui = FALSE
+         , out_folder = "LandCoverData"
+         , out_folder_mod = "LandCoverData"
+         , selprod = "LandCover_Type_Yearly_500m (MCD12Q1)"
+         , bandsel = "LC1"
+         , use = "briancalhoon"
+         , password = "00h0OqKWDw$67R"
+         , start_date = "2020.01.01"
+         , end_date = "2020.12.31"
+         , verbose = FALSE
+         , spatmeth = "file"
+         , spafile = spatial_filepath
+         , out_format = "GTIFF")
+
 
 IGBP_raster <- raster(here::here("LandCoverData/france/LandCover_Type_Yearly_500m_v6/LC1/MCD12Q1_LC1_2020_001.tif"))

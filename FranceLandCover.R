7890b9557d2056749ab79ff78666dde18c21c4b8
Brian Calhoon <bcalhoon@msi-inc.com>
2021-11-15 16:08:49 -0500

    script

--- FranceLandCover.R
+++ FranceLandCover.R
@@ -37,4 +37,48 @@ MODIStsp(gui = FALSE
          , out_format = "GTIFF")
 
 
+# Downloading the boundary of Zimbabwe
+map_boundary <- geoboundaries("Zimbabwe")
+
+# Reading in the downloaded landcover raster data
 IGBP_raster <- raster(here::here("LandCoverData/france/LandCover_Type_Yearly_500m_v6/LC1/MCD12Q1_LC1_2020_001.tif"))
+
+# Transforming data
+IGBP_raster <- projectRaster(IGBP_raster, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+# Cropping data
+IGBP_raster <- raster::mask(IGBP_raster, as_Spatial(map_boundary))
+
+# Converting the raster object into a dataframe and converting the IGBP classification into a factor
+IGBP_df <- as.data.frame(IGBP_raster, xy = TRUE, na.rm = TRUE) %>%
+  mutate(MCD12Q1_LC1_2020_001 = as.factor(round(MCD12Q1_LC1_2020_001)))
+rownames(IGBP_df) <- c()
+# Renaming IGBP classification levels
+levels(IGBP_df$MCD12Q1_LC1_2020_001) <- c( "Evergreen needleleaf forests",
+                                           "Evergreen broadleaf forests",
+                                           "Deciduous needleleaf forests",
+                                           "Deciduous broadleaf forests",
+                                           "Mixed forests",
+                                           "Closed shrublands",
+                                           "Open shrublands",
+                                           "Woody savannas",
+                                           "Savannas",
+                                           "Grasslands",
+                                           "Permanent wetlands",
+                                           "Croplands",
+                                           "Urban and built-up lands",
+                                           "Cropland/natural vegetation mosaics",
+                                           "Snow and ice",
+                                           "Barren",
+                                           "Water bodies")
+# Visualising using ggplot2
+ggplot() + 
+  geom_raster(data = IGBP_df,
+              aes(x = x, y = y, fill = MCD12Q1_LC1_2020_001)) +
+  geom_sf(data = map_boundary, inherit.aes = FALSE, fill = NA) +
+  scale_fill_viridis(name = "Land Cover Type", discrete = TRUE) +
+  labs(title = "Land Cover classification in France",
+       subtitle = "1 janvier 2020 - 21 decembre 2020",
+       x = "Longitude",
+       y = "Latitude") +
+  theme_minimal()
